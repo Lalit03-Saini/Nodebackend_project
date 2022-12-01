b34a9cae934ff1cb59dd7e12b968395221afe170
Lalit03-Saini <lalitdaiya54@gmail.com>
2022-12-01 18:50:06 +0530

    Updating

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ const app = express()
 const DataRouter = require("./Routes/data")
 const cors = require("cors")
 
-// app.get("/home", function (req,res) {
+app.get("/home", function (req,res) {
 
-//     res.send("Home")
-// })
+    res.send("Home")
+})
 
 app.use(cors())
-app.use("/api",DataRouter)
+app.get("/details",DataRouter)
 
 app.listen(process.env.PORT||8080,function () {
     console.log('Application is Running')
